5e6b845b26112c681858e31273bb2640b74354bd
borney11 <sidSidhant50@gmail.com>
2026-04-22 15:34:28 +0530

    Update detect.js

--- frontend/pages/detect.js
+++ frontend/pages/detect.js
@@ -39,7 +39,7 @@ export default function Detect() {
       const formData = new FormData();
       formData.append("file", blob);
 
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://emotion-app-aqcx.onrender.com/predict", {
         method: "POST",
         body: formData
       });
